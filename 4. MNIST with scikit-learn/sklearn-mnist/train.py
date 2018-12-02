ad9532eac8543b997b4ce41e45b8d96056707583
David Smith <davidsmi@microsoft.com>
2018-12-02 16:53:55 -0600

    Added MNIST scikit-learn example

---
+++ 4. MNIST with scikit-learn/sklearn-mnist/train.py
@@ -0,0 +1,48 @@
+
+import argparse
+import os
+import numpy as np
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.externals import joblib
+
+from azureml.core import Run
+from utils import load_data
+
+# let user feed in 2 parameters, the location of the data files (from datastore), and the regularization rate of the logistic regression model
+parser = argparse.ArgumentParser()
+parser.add_argument('--data-folder', type=str, dest='data_folder', help='data folder mounting point')
+parser.add_argument('--regularization', type=float, dest='reg', default=0.01, help='regularization rate')
+args = parser.parse_args()
+
+data_folder = os.path.join(args.data_folder, 'mnist')
+print('Data folder:', data_folder)
+
+# load train and test set into numpy arrays
+# note we scale the pixel intensity values to 0-1 (by dividing it with 255.0) so the model can converge faster.
+X_train = load_data(os.path.join(data_folder, 'train-images.gz'), False) / 255.0
+X_test = load_data(os.path.join(data_folder, 'test-images.gz'), False) / 255.0
+y_train = load_data(os.path.join(data_folder, 'train-labels.gz'), True).reshape(-1)
+y_test = load_data(os.path.join(data_folder, 'test-labels.gz'), True).reshape(-1)
+print(X_train.shape, y_train.shape, X_test.shape, y_test.shape, sep = '\n')
+
+# get hold of the current run
+run = Run.get_context()
+
+print('Train a logistic regression model with regularizaion rate of', args.reg)
+clf = LogisticRegression(C=1.0/args.reg, random_state=42)
+clf.fit(X_train, y_train)
+
+print('Predict the test set')
+y_hat = clf.predict(X_test)
+
+# calculate accuracy on the prediction
+acc = np.average(y_hat == y_test)
+print('Accuracy is', acc)
+
+run.log('regularization rate', np.float(args.reg))
+run.log('accuracy', np.float(acc))
+
+os.makedirs('outputs', exist_ok=True)
+# note file saved in the outputs folder is automatically uploaded into experiment record
+joblib.dump(value=clf, filename='outputs/sklearn_mnist_model.pkl')
